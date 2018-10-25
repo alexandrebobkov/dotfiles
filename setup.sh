5f0b591ce61196128c25360c0cd323736d86ab05
Douglas Alan Maitelli <dougmaitelli@gmail.com>
2018-10-25 14:15:17 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setting up dotfiles
+echo "Setting up dotfiles"
+git clone --bare https://github.com/dougmaitelli/dotfiles.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout
+fi;
+config config status.showUntrackedFiles no
+rm -rf ~/.README.md
+config ls-files --deleted -z | config update-index --assume-unchanged -z --stdin
