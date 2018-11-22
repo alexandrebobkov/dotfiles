f96fe1f096934012e4a1d366914ce500ba76b9d6
Douglas Maitelli <dougmaitelli@gmail.com>
2018-11-22 16:15:20 +0100

    Migrating to Compton

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,10 +40,3 @@ end
 set -x NPM_PACKAGES $HOME/.npm-packages
 set -x PATH $NPM_PACKAGES/bin $PATH
 set -x MANPATH $NPM_PACKAGES/share/man (manpath)
-
-set -l ppid (cat /proc/(echo %self)/status | grep PPid | cut -f2)
-if string match -r 'tilix' (ps --no-headers -p $ppid -o comm) 1> /dev/null
-    for wid in (xdotool search --pid $ppid)
-        xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $wid
-    end
-end
