2ab78f0bacca1764ce3cd6b479e0971504ec0b5c
Douglas Maitelli <dougmaitelli@gmail.com>
2018-12-16 20:34:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,12 +19,12 @@ git clone --bare https://github.com/dougmaitelli/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
+    mkdir -p .config-backup
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
     config checkout
 fi;
